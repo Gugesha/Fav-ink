254d8e0e114c1b803db3d2f6dc41d2d80fc7d20a
Gugesha <guge11@live.com>
2023-06-03 16:23:09 +0400

    fav-ink

---
+++ script.js
@@ -0,0 +1,7 @@
+let navigation = document.getElementById ('navigation1');
+let burgerbar = document.getElementById('burger');
+
+burgerbar.addEventListener('click', function() {
+    navigation.classList.toggle('active');
+});
+
